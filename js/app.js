15102e0e81fb6d0137ea62ae1df91cd62b861196
Bartlomiej Szywala <czarny245@gmail.com>
2019-01-18 12:52:19 +0000

    Separate data from the main app.js file, fixed the image error

--- js/app.js
+++ js/app.js
@@ -5,70 +5,6 @@ var map;
 var CLIENT_ID = "WYJ23K5PKH2RCV1UCESCNSPPO3UAQJHSWHORAP2AAYTEQAJA";
 var CLIENT_SECRET = "YNMGAPQW4ZADXENOYO5T2XLZDFKJYLTHTUOEU102BO13ZDLJ";
 
-// Data containing location examples
-var places = [
-    {
-    name: "PadelClub London",
-    lat: 51.502765787443316,
-    lng: -0.011341527993155728,
-    id: "4f86c408e4b02b999bba04b7"
-    },
-    {
-    name: "Tower Hamlets Tennis Court",
-    lat: 51.54022455242505,
-    lng: -0.03371760529630305,
-    id: "51e249bc498e74d13a39d410"
-    },
-    {
-    name: "Poplar Park Tennis Courts",
-    lat: 51.51037908256234,
-    lng: -0.01704836039394682,
-    id: "54437af8498edf77dad86c02"
-    },
-    {
-    name: "Southwark Park",
-    lat: 51.49498975352677,
-    lng: -0.0565382708983765,
-    id: "4b5f5f31f964a5209eb629e3"
-    },
-    {
-    name: "tennis court @ king edward memorial park",
-    lat: 51.50869139968924,
-    lng: -0.04926191262009084,
-    id: "5210d64d11d25708a006651c"
-    },
-    {
-    name: "Mellish Fields Community Sports Ground",
-    lat: 51.5026180102039,
-    lng: -0.042106579792284514,
-    id: "508e7970e4b040090bbe8cc8"
-    },
-    {
-    name: "St John's Park Tennis Courts",
-    lat: 51.497345,
-    lng: -0.010855,
-    id: "5741af3e498e439899200a23"
-    },
-    {
-    name: "Bethnal Green Gardens Tennis Courts",
-    lat: 51.52523975195817,
-    lng: -0.05452246712701292,
-    id: "4eb1e95b775b97929411e9f5"
-    },
-    {
-    name: "Haggerston Park Tennis Courts",
-    lat: 51.532318018743894,
-    lng: -0.06771779898433523,
-    id: "522c3d1611d2c4632297c3f5"
-    },
-    {
-    name: "Hermit Road Park",
-    lat: 51.524804417223756,
-    lng: 0.013750280992246736,
-    id: "5188b896498e8cc9b8efe9ba"
-    }
-];
-
 // Initialize the map and the ViewModel
 function initMap() {
     mapElement = $("#map")
@@ -172,7 +108,9 @@ var ViewModel = function() {
                     if (placeItem.contact()) {
                         infoStr += '<p>'+placeItem.contact()+'</p>'
                     }
-                    if (placeItem.image()) {
+                    if (placeItem.image().indexOf("undef") > -1) {
+                        infoStr += '<p>'+"There was no photo for this place in Forsquare database"+'</p>'
+                    } else {
                         infoStr += '<img src="'+placeItem.image()+'" alt="No image in forsquare database">'
                     }
                     if (placeItem.url()) {
@@ -189,7 +127,7 @@ var ViewModel = function() {
                     placeItem.info = infoStr;
                     placeItem.marker = marker;
                 },
-                // This function will inform the user that something went wrong with forsquare(inside an infoWindow)
+                // This function will inform the user 
                 error: function () {
                     marker.addListener('click', function(){
                         infoWindow.open(map, marker);
